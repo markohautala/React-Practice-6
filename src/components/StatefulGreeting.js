7862e446ea523f75bb85d448f2afc6a3452456cb
markohautala <markohautala@live.se>
2024-06-25 17:52:51 +0300

    Update App.js and StatefulGreeting.js

--- src/components/StatefulGreeting.js
+++ src/components/StatefulGreeting.js
@@ -1,2 +1,9 @@
 import React from 'react';
 
+class StatefulGreeting extends React.Component {
+    render() {
+        return <h1> {this.props.greeting} </h1>
+    }
+}
+
+export default StatefulGreeting;
