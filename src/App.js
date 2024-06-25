e34734b502fc989a0658ad1ad6124bffea9b1d2d
markohautala <markohautala@live.se>
2024-06-25 18:25:57 +0300

    Add state and props to the rendered JSX

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import StatefulGreeting from './components/StatefulGreeting';
 function App() {
   return (
     <div className="App">
-      <StatefulGreeting greeting="This is now the greeting as a prop"/>
+      <StatefulGreeting greeting="This is now the greeting as a prop from app.js"/>
     </div>
   );
 }
--- src/components/StatefulGreeting.js
+++ src/components/StatefulGreeting.js
@@ -1,8 +1,23 @@
 import React from 'react';
 
 class StatefulGreeting extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            introduction: "This is a prop from the constructor",
+            buttonText: "Click me"
+
+        };
+    }
+
     render() {
-        return <h1> {this.props.greeting} </h1>
+        return (
+            <div>
+                <h1> {this.state.introduction} {this.props.greeting} </h1>
+                <button> {this.state.buttonText} </button>
+            </div>
+        )
     }
 }
 
